82d358e7db40280c53f47dd0168927e58c45577f
ernie3000 <50182266+ernie3000@users.noreply.github.com>
2019-07-01 16:15:33 +0200

    bugfix
    fix

--- js/scripts.js
+++ js/scripts.js
@@ -2,6 +2,7 @@
 var pokemonRepository = (function () {
   var repository = [];
   var apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
+ 
 
 
   function loadList() {
@@ -59,58 +60,26 @@ pokemonRepository.loadList().then(function() {
 var pokemonRepositoryNew = pokemonRepository.getAll();
 
 
- //add-list-item function:
-
-
-
-
-
- function addListItem(pokemon) {
-  var $modal = $('<div class="modal-container"></div>')
-  var $NameButton = $('<button class="Name-button">' + pokemon.name + '</button>');
-  
-  
-
-  var $li = $('<li class="list-item"></li>');
-  var $ul = $('ul');
-  var listItemheight =$('<div>'+ pokemon.height + '</div>');
-  var listItemtypes =$('<div>'+ pokemon.types + '</div>');  
-
-  
+function addListItem(pokemon) {
+  var $NameButton = $('<button class="name-button">' + pokemon.name + '</button>');
+  var $li = $('<div class="list-item"></div>');
+  var $modal = $('<div class="modal-container"></div>');
+  var $ul = $('<ul></ul>');
   $("#Poke-List").append($li); 
   $("#modal-container").append($modal);
   $ul.append($modal);
   $li.append($NameButton);
+
+  $NameButton.on('click', function(event){
+    showDetails(pokemon)});
  
-
-
- 
- var listItemheight = $('<div>'+ pokemon.height + '</div>'); 
- var listItemtypes = $('<div>' + pokemon.types +'</div>'); 
- 
- 
-
- //$NameButton.on('click', function(event){
- // console.log('test pokemon');
-//});
-$NameButton.on('click', function(event){
-  showDetails(pokemon);
-});
-
-
 };
-//------------------------------------------------------------//
 
-
-
-//loop.creating.pokemons.from.repository
-
-
+// Creating List and Buttons
 
 $(pokemonRepositoryNew).each(function(pokemon) {
   addListItem(pokemon)
 });
-
 
 // Showing Modal with Picture / Height / Name
 
@@ -120,29 +89,51 @@ function showDetails(item) {
 };
 
 
-
-//-----------------------------
-// creating modal content
 function showModal(item) {
-  var $modalContainer = $('<div></div>');
-  $($modalContainer).appendTo('#modal-container');
-  $($modalContainer).addClass( "is-visible");
-  var $modal = $('<div></div>');
-  $($modal).addClass("modal");
-  var closeButtonElement =$('<button class="modal-close"> Close </button>');
-  var nameElement = $('<h1>' + item.name + '</h1>');
-  var imageElement = $('<img class="modal-img">' + item.imageUrl +'</img>');
-  var heightElement = $('<p>' + item.height + '</p>');
-  
+  var $modalContainer = $('#modal-container');
+  $modalContainer.show();
+  $('#modal-container').html('');
 
+ var modal = $('<div class="modal"></div>');
   
-  $(closeButtonElement).appendTo('$modal');
-  $(nameElement).appendTo('$modal');
-  $(imageElement).appendTo('$modal');
-  $(heightElement).appendTo('$modal');
-  $($modal).appendTo('$modalcontainer');
+  var closeButtonElement = $('<button class="modal-close">Close</button>');
+  var nameElement = $('<h1>' + item.name + '</h1>');
   
+  var imageElement = $('<img class="modal-image"></img>');
+  imageElement.attr("src",item.imageUrl);    //attribute set in jquery format
   
-  
+  var heightElement = $('<p>' + item.height + '</p>');
+
+  var modalContent = $('<div class = "modal-content">');
+
+ //appending element to modal and modal to modal container
+
+
+  modalContent.append(closeButtonElement).append(nameElement).append(imageElement).append(heightElement);
+ 
+  modal.append(modalContent);
+  $modalContainer.append(modal).addClass('is-visible');
+
+  closeButtonElement.on('click', function(event){
+    $modalContainer.hide()});
+
+    //hide modal when ESC on keyboard is pressed down
+ 
+    $(window).on('keydown', (e) => {
+      if (e.key === 'Escape' && $('#modal-container').hasClass('is-visible')) {
+         $modalContainer.hide();
+      }
+    });
+ 
+    //hide modal if clicked outside of it  #9
+ 
+    $('#modal-container').on('click', function (event) {
+      if ($(event.target).is($modalContainer)) {
+        $modalContainer.hide();
+      }
+    });
+
+
+
 };
-  
+
